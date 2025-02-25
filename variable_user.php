9064236e039a8b1c1fd0da7eb2444a8f94820075
muhammadjafar986 <muhammadjafarshodiq65@gmail.com>
2025-02-25 12:37:52 +0700

    upload web-2

---
+++ variable_user.php
@@ -0,0 +1,11 @@
+<?php
+//mendefinisikan variable
+$nama = 'Jafar';
+$umur = 18;
+$berat = 48.15;
+
+echo 'Nama saya ' . $nama;
+echo '<br/>Umur: ' . $umur . 'Tahun';
+echo '<br/>Berat: ' . $berat . 'Kg';
+
+?>
