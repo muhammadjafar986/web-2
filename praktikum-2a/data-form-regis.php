2259753054382bb93a9e83efdab71cd4e47cf7f4
muhammadjafar986 <muhammadjafarshodiq65@gmail.com>
2025-03-04 11:06:53 +0700

    Add files via upload

---
+++ praktikum-2a/data-form-regis.php
@@ -0,0 +1,18 @@
+<?php
+$ar_prodi = [
+    'SI' => 'Sistem Informasi',
+    'TI' => 'Teknik Informatika',
+    'BD' => 'Bisnis Digital'
+];
+
+$ar_skill = [
+    'HTML' => 10,
+    'CSS' => 9,
+    'Javacript' => 8,
+    'RWD Bootstrap' => 20,
+    'PHP' => 30,
+    'Pyhthon' => 30,
+    'Java' => 50
+];
+
+$ar_domisili =["Jakarta", "Depok", "Bogor", "Tangerang", "Bekasi","Lainnya"];
